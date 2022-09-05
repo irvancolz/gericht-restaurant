f4fcca690fb39f3607a1a132ed054d82333f3713
Irvan Saharudin <irvansaharudin@gmail.com>
2022-09-05 22:05:23 +0700

    finish on creating topnav

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,7 +1,29 @@
-export function Button(){
-    return(
-        <button>
-            test
-        </button>
-    )
+import styles from "./button.module.css";
+interface buttonProps {
+  variant?: string;
+  children?: string | JSX.Element[] | JSX.Element;
+  onClick?: () => void;
+}
+const buttonDefaultProps: buttonProps = {
+  variant: "primary",
+  onClick: () => {
+    return;
+  },
+  children: "Actions",
+};
+export function Button(props: buttonProps = buttonDefaultProps) {
+  return (
+    <button
+      className={`${styles.btn} ${
+        props.variant === "primary"
+          ? styles.primary
+          : props.variant === "secondary"
+          ? styles.secondary
+          : styles.primary
+      }`}
+      onClick={() => props.onClick?.()}
+    >
+      {props.children}
+    </button>
+  );
 }
--- components/topnav/index.tsx
+++ components/topnav/index.tsx
@@ -3,35 +3,36 @@ import Link from "next/link";
 import { useState } from "react";
 import { Section } from "../section";
 import styles from "./topnav.module.css";
-import webLogo from '../../public/assets/global/web-logo.png'
+import webLogo from "../../public/assets/global/web-logo.svg";
+import { Button } from "../button";
 
 const navigationLinks = [
-    {
-        id: 0,
-        label: 'Home',
-        link: '/',
-    },
-    {
-        id: 1,
-        label: 'Pages',
-        link: '/pages',
-    },
-    {
-        id: 2,
-        label: 'Contact Us',
-        link: '/contactus',
-    },
-    {
-        id: 3,
-        label: 'Blog',
-        link: '/blog',
-    },
-    {
-        id: 4,
-        label: 'Landing',
-        link: '/landing',
-    },
-]
+  {
+    id: 0,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 1,
+    label: "Pages",
+    link: "/pages",
+  },
+  {
+    id: 2,
+    label: "Contact Us",
+    link: "/contactus",
+  },
+  {
+    id: 3,
+    label: "Blog",
+    link: "/blog",
+  },
+  {
+    id: 4,
+    label: "Landing",
+    link: "/landing",
+  },
+];
 
 export function Topnav() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -42,31 +43,45 @@ export function Topnav() {
   return (
     <Section justify="space-between" paddingX="2rem" paddingY="1rem">
       <div className={styles.logoC}>
-        <div>
-          <Link href='/'>
+        <div className={styles.logo}>
+          <Link href="/" as="/home">
             <a>
-              <Image src={webLogo} alt='gericht' layout="intrinsic" priority/>
+              <Image src={webLogo} alt="gericht" layout="intrinsic" priority />
             </a>
           </Link>
         </div>
-        <span onClick={() => toggleOpen()} className={styles.toggleI}>&#9776;</span>
-      </div>
-      <div className={styles.mainC}>
-        <nav>
-            {navigationLinks.map(item =>{
-                return(
-                    <span key={item.id} className={styles.link}>
-                        <Link href={item.link}>{item.label}</Link>
-                    </span>
-                )
-            })}
-        </nav>
-      </div>
-      <div className={styles.regsC}>
-        <div className="regs">
-            
+        {/* Show on tablet */}
+        <div className={styles.regs}>
+          <Button variant="ternary">
+            <span className={styles.link}>Log In / Regristration</span>
+          </Button>
+          <Button variant="ternary">
+            <span className={styles.link}>Book Table</span>
+          </Button>
         </div>
-        <span onClick={() => toggleOpen()} className={styles.toggleI}>&#9776;</span>
+        <Button onClick={() => toggleOpen()}>
+          <span className={styles.toggleI}>&#9776;</span>
+        </Button>
+      </div>
+      <div className={`${styles.mainC} ${isOpen && styles.open}`}>
+        <nav>
+          {navigationLinks.map((item) => {
+            return (
+              <span key={item.id} className={styles.link}>
+                <Link href={item.link}>{item.label}</Link>
+              </span>
+            );
+          })}
+        </nav>
+        {/* show on mobile */}
+        <div className={styles.regs}>
+          <Button variant="ternary">
+            <span className={styles.link}>Log In / Regristration</span>
+          </Button>
+          <Button variant="ternary">
+            <span className={styles.link}>Book Table</span>
+          </Button>
+        </div>
       </div>
     </Section>
   );
