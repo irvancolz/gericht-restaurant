c9d3649d5bbb77efb62b3e33519c2ee3f054477d
Irvan Saharudin <irvansaharudin@gmail.com>
2022-10-04 21:05:46 +0700

    fix footer content responsive order

--- ui/footer content/footer-content.style.ts
+++ ui/footer content/footer-content.style.ts
@@ -1,48 +1,69 @@
 import { css } from "../../stitches.config";
 
 export const ContainerStyles = css({
-    mt: "$7",
-    mb: "$6",
-    display: "flex",
-    align: "flex-start",
-    justify: "space-between",
-    gap: "$2",
+  mt: "$7",
+  mb: "$6",
+  display: "flex",
+  align: "flex-start",
+  justify: "space-between",
+  gap: "$2",
 
-    "@bp2" :{
-        fDir: "column",
-    },
+  "@bp2": {
+    fWrap: "wrap",
+    gap: "$3",
+    mt: "$4",
+    mb: "$3",
+  },
+  "@bp1":{
+    gap: "$4",
+  }
 });
 
 export const ContentStyles = css({
-    display: "flex",
-    fDir:"column",
-    justify: "center",
-    align: "center",
+  display: "flex",
+  fDir: "column",
+  justify: "center",
+  align: "center",
 
-    "&:nth-child(2)" :{
-        fBase: "100%",
-    },
+  "&:nth-child(2)": {
+    fBase: "100%",
+  },
 
-    variants: {
-        space: {
-            sm: {
-                gap: "$2",
-            },
-            md: {
-                gap: "$4",
-            },
-        },
+  variants: {
+    space: {
+      sm: {
+        gap: "$2",
+      },
+      md: {
+        gap: "$4",
+      },
     },
-    defaultVariants:{
-        space : "sm"
-    }
+  },
+
+  "@bp2": {
+    "&:nth-child(2)": {
+      order: "0",
+    },
+    "&:not(:nth-child(2))": {
+      order: "2",
+      fBase: "calc(50% - 8px)",
+    },
+  },
+  "@bp1": {
+    "&:not(:nth-child(2))": {
+      fBase: "100%",
+    },
+  },
+  defaultVariants: {
+    space: "sm",
+  },
 });
 
 export const TextStyles = css({
-    mb: '$1',
+  mb: "$1",
 });
 
 export const ImageLinkStyles = css({
-    display: "flex",
-    gap: "$2",
-})
+  display: "flex",
+  gap: "$2",
+});
