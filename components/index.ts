fe69125b17d5e7c4db515f402f1cdaaeefbaa0b9
Irvan Saharudin <irvansaharudin@gmail.com>
2022-10-27 15:18:04 +0700

    fix #60dd96e

--- components/index.ts
+++ components/index.ts
@@ -4,14 +4,13 @@ import { Footer } from "./footer";
 import { Layout } from "./layout";
 import { NestedLayout } from "./layout nested";
 import { Topnav } from "./topnav";
-import { Article_Header } from "./article header";
+export * from "./article header";
 
-export  const Components ={
-    Copyright,
-    Footer,
-    Layout,
-    NestedLayout,
-    Topnav,
-    Carousel,    
-    Article_Header
-}
+export const Components = {
+  Copyright,
+  Footer,
+  Layout,
+  NestedLayout,
+  Topnav,
+  Carousel,
+};
