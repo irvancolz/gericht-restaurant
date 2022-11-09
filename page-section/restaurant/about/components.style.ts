656e72b3613303a7c956444127df4056fcd87fcf
Irvan Saharudin <irvansaharudin@gmail.com>
2022-11-09 17:10:32 +0700

    add restaturants about bg assets

--- page-section/restaurant/about/components.style.ts
+++ page-section/restaurant/about/components.style.ts
@@ -1,5 +1,21 @@
 import { styled } from "../../../stitches.config";
-import { Box } from "../../../ui";
-export const HeroAboutContainerStyles = styled(Box,{
+import { Box, Section } from "../../../ui";
 
-})
+export const HeroAboutContainerStyles = styled(Box, {
+  display: "flex",
+  gap: "85px",
+  py: "$8",
+  justify: "center",
+  align: "center",
+});
+
+export const AboutRestaurantsStyles = styled(Section, {
+  variants: {
+    content: {
+      right: {
+        display: "grid",
+        placeItems: "end",
+      },
+    },
+  },
+});
--- page-section/restaurant/about/components.tsx
+++ page-section/restaurant/about/components.tsx
@@ -1,17 +1,61 @@
+import Image from "next/image";
 import React from "react";
-import { ArticleHeader, ArticleHeading, ArticleTitle } from "../../../components";
-import { Box, Section } from "../../../ui";
+import { ArticleHeader, ArticleHeading } from "../../../components";
+import { Box, Section, Text, Button } from "../../../ui";
+import knivesSrc from "../../../public/assets/restaurants/restaurant-knives-img.png";
+import { HeroAboutContainerStyles, AboutRestaurantsStyles } from "./components.style";
 
 export function HeroAbout() {
   return (
-    <Box paddingSide="lg">
-      <Section>
+    <HeroAboutContainerStyles paddingSide="lg">
+      <AboutRestaurantsStyles content="right">
         <ArticleHeader content="right">
           <ArticleHeading as="h2" fCol="gold" size="lg">
             About Us
           </ArticleHeading>
         </ArticleHeader>
-      </Section>
-    </Box>
+        <Box>
+          <Text
+            css={{
+              my: "$4",
+            }}
+            fCol="fade"
+            align="end"
+            family="open"
+            size="sm"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+            pharetra adipiscing ultrices vulputate posuere tristique. In sed
+            odio nec aliquet eu proin mauris et.
+          </Text>
+          <Button>Know More</Button>
+        </Box>
+      </AboutRestaurantsStyles>
+      <Box>
+        <Image src={knivesSrc} layout="fixed" alt="spoon" />
+      </Box>
+      <AboutRestaurantsStyles>
+        <ArticleHeader>
+          <ArticleHeading as="h2" fCol="gold" size="lg">
+            Our History
+          </ArticleHeading>
+        </ArticleHeader>
+        <Box>
+          <Text
+            css={{
+              my: "$4",
+            }}
+            fCol="fade"
+            family="open"
+            size="sm"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+            pharetra adipiscing ultrices vulputate posuere tristique. In sed
+            odio nec aliquet eu proin mauris et.
+          </Text>
+          <Button>Know More</Button>
+        </Box>
+      </AboutRestaurantsStyles>
+    </HeroAboutContainerStyles>
   );
 }
